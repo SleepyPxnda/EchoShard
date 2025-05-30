07c3daca92deeb8ef0f797665fed4bcc2855d266
SleepyPxnda <44275698+SleepyPxnda@users.noreply.github.com>
2025-05-31 00:07:03 +0200

    Initial Money Setup

---
+++ plugin/src/main/java/de/cloudypanda/echoShard/EchoShard.java
@@ -0,0 +1,17 @@
+package de.cloudypanda.echoShard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EchoShard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
